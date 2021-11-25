755ed43c390c87fc3186dfbe4a31625bc5c0b21e
RobertSolovev39 <RobertSolovev39@gmail.com>
2021-11-25 23:45:10 +0200

    Update update_schemas.py

--- src/autograph/services/core/update_schemas.py
+++ src/autograph/services/core/update_schemas.py
@@ -6,8 +6,10 @@ def update_schemas() -> bool:
     ''' Обновление схем '''
 
     existing_schemas = Schema.objects.values_list('schema_id', flat=True)
+
     schemas = [
         Schema(schema_id=schema['ID'], name=schema['Name'])
     for schema in autograph_session.get_schemas() if schema['ID'] not in existing_schemas]
     Schema.objects.bulk_create(schemas)
+
     return True
