2d7f708854b26860a546640e74c7a1297fc7bc57
RobertSolovev39 <RobertSolovev39@gmail.com>
2021-11-25 18:04:25 +0200

    Deleted test tasks

--- src/settings/celery.py
+++ src/settings/celery.py
@@ -6,8 +6,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.settings')
 app = Celery('settings')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
-
-@app.task(name='test_task')
-def test_task() -> bool:
-    return True
