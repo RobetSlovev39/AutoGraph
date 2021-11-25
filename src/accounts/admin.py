675a75e170fa7ef15f9af591e1d815dd266ded7b
RobertSolovev39 <RobertSolovev39@gmail.com>
2021-11-25 17:42:24 +0200

    Started Django project

---
+++ src/accounts/admin.py
@@ -0,0 +1,9 @@
+from .models import User
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    pass
